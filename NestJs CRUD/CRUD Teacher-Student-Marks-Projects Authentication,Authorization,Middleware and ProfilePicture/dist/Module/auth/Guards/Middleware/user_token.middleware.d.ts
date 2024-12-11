b4a6704f7b1f8d35fee67092b65a45672e42e466
Santhosh L <santhoshgl1999@gmail.com>
2024-12-11 16:25:09 +0530

    Add files via upload

--- NestJs CRUD/CRUD Teacher-Student-Marks-Projects Authentication,Authorization,Middleware and ProfilePicture/dist/Module/auth/Guards/Middleware/user_token.middleware.d.ts
+++ NestJs CRUD/CRUD Teacher-Student-Marks-Projects Authentication,Authorization,Middleware and ProfilePicture/dist/Module/auth/Guards/Middleware/user_token.middleware.d.ts
@@ -1,10 +1,6 @@
 import { NestMiddleware } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { Request, Response, NextFunction } from 'express';
-import { UsersService } from 'src/Module/user/users.service';
 export declare class JwtMiddleware implements NestMiddleware {
-    private readonly jwtService;
-    private userService;
-    constructor(jwtService: JwtService, userService: UsersService);
+    constructor();
     use(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
--- NestJs CRUD/CRUD Teacher-Student-Marks-Projects Authentication,Authorization,Middleware and ProfilePicture/dist/Module/auth/Guards/Middleware/user_token.middleware.js
+++ NestJs CRUD/CRUD Teacher-Student-Marks-Projects Authentication,Authorization,Middleware and ProfilePicture/dist/Module/auth/Guards/Middleware/user_token.middleware.js
@@ -6,11 +6,17 @@ const jwt = require("jsonwebtoken");
 const auth_constants_1 = require("../../auth.constants");
 const Route_role_1 = require("./Route.role");
 class JwtMiddleware {
-    constructor(jwtService, userService) {
-        this.jwtService = jwtService;
-        this.userService = userService;
-    }
+    constructor() { }
     async use(req, res, next) {
+        const { method, originalUrl } = req;
+        const requestStartTime = new Date().getTime();
+        res.on('finish', () => {
+            const { statusCode } = res;
+            const responseTime = new Date().getTime();
+            const duration = responseTime - requestStartTime;
+            const message = `${method} ${originalUrl} - ${statusCode} ${duration}ms`;
+            console.log(message);
+        });
         const authHeader = req.headers['authorization'];
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
             throw new common_1.UnauthorizedException('No token provided');
@@ -24,7 +30,7 @@ class JwtMiddleware {
             console.log('Object from middleware: ', req.user);
             const matchedRole = Object.keys(Route_role_1.routeRole).find((key) => req.path.startsWith(key));
             const requiredRole = matchedRole ? Route_role_1.routeRole[matchedRole] : undefined;
-            console.log(requiredRole);
+            console.log("Required Role: ", requiredRole);
             if (requiredRole && !requiredRole.includes(decoded.role)) {
                 throw new common_1.ForbiddenException('Access denied');
             }
