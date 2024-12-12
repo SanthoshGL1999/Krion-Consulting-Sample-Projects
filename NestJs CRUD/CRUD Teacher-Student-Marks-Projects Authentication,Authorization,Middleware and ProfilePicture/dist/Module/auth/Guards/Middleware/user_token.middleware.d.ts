a2798e6223f9c02b2f1811645946b70e66efd3f9
Santhosh L <santhoshgl1999@gmail.com>
2024-12-12 12:18:49 +0530

    Add files via upload

--- NestJs CRUD/CRUD Teacher-Student-Marks-Projects Authentication,Authorization,Middleware and ProfilePicture/dist/Module/auth/Guards/Middleware/user_token.middleware.d.ts
+++ NestJs CRUD/CRUD Teacher-Student-Marks-Projects Authentication,Authorization,Middleware and ProfilePicture/dist/Module/auth/Guards/Middleware/user_token.middleware.d.ts
@@ -1,6 +1,5 @@
 import { NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 export declare class JwtMiddleware implements NestMiddleware {
-    constructor();
     use(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
--- NestJs CRUD/CRUD Teacher-Student-Marks-Projects Authentication,Authorization,Middleware and ProfilePicture/dist/Module/auth/Guards/Middleware/user_token.middleware.js
+++ NestJs CRUD/CRUD Teacher-Student-Marks-Projects Authentication,Authorization,Middleware and ProfilePicture/dist/Module/auth/Guards/Middleware/user_token.middleware.js
@@ -6,8 +6,13 @@ const jwt = require("jsonwebtoken");
 const auth_constants_1 = require("../../auth.constants");
 const Route_role_1 = require("./Route.role");
 class JwtMiddleware {
-    constructor() { }
     async use(req, res, next) {
+        const start = Date.now();
+        res.on('finish', () => {
+            const end = Date.now();
+            const duration = end - start;
+            console.log(`${req.method} ${req.originalUrl} - ${res.statusCode} [${duration}ms]`);
+        });
         const { method, originalUrl } = req;
         const requestStartTime = new Date().getTime();
         res.on('finish', () => {
@@ -24,13 +29,9 @@ class JwtMiddleware {
         const token = authHeader.split(' ')[1];
         try {
             const decoded = jwt.verify(token, auth_constants_1.jwtConstants.secret);
-            console.log("Token is: ", token);
-            console.log("Secret-Key is: ", auth_constants_1.jwtConstants.secret);
             req['user'] = decoded;
-            console.log('Object from middleware: ', req.user);
             const matchedRole = Object.keys(Route_role_1.routeRole).find((key) => req.path.startsWith(key));
             const requiredRole = matchedRole ? Route_role_1.routeRole[matchedRole] : undefined;
-            console.log("Required Role: ", requiredRole);
             if (requiredRole && !requiredRole.includes(decoded.role)) {
                 throw new common_1.ForbiddenException('Access denied');
             }
